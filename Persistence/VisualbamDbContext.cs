d2b424460df6dac3cf33c1884d1843e87c97fff7
Bruce McElroy <bruce.mcelroy@Gmail.com>
2018-03-11 21:39:40 -0400

    add db context

---
+++ Persistence/VisualbamDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using visualbam.Models;
+
+namespace visualbam.Persistence
+{
+    public class VisualbamDbContext : DbContext
+    {
+        public VisualbamDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using visualbam.Persistence;
 
 namespace visualbam
 {
@@ -17,11 +20,12 @@ namespace visualbam
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VisualbamDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
         }
 
