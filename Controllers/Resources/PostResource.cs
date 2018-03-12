c884f2554ef88ffbb69cdecb628caa4e6e941eaf
Bruce McElroy <bruce.mcelroy@Gmail.com>
2018-03-11 21:40:02 -0400

    add posts

---
+++ Controllers/Resources/PostResource.cs
@@ -0,0 +1,10 @@
+﻿namespace visualbam.Controllers
+{
+    public class PostResource
+    {
+        public int post_id { get; set; }
+        public string title { get; set; }
+        public string category { get; set; }
+        public string content { get; set; }
+    }
+}
--- Models/Post.cs
+++ Models/Post.cs
@@ -1,10 +1,24 @@
-﻿namespace visualbam.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace visualbam.Models
 {
+    [Table("Posts")]
     public class Post
     {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Category { get; set; }
+        [Key]
+        [Required]
+        public int post_id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string title { get; set; }
+
+        [Required]
+        [StringLength(5000)]
+        public string content { get; set; }
+
+        [Required]
+        public string category { get; set; }
     }
 }
