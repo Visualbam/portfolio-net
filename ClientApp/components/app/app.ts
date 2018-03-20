9706a1571818949921e986fc2f257971def1cadf
Bruce McElroy <bruce.mcelroy@Gmail.com>
2018-03-20 00:54:02 -0400

    adds navigation

--- ClientApp/components/app/app.ts
+++ ClientApp/components/app/app.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
+import { Component, Prop, Watch } from 'vue-property-decorator';
 
 @Component({
     components: {
@@ -7,9 +7,31 @@ import { Component, Prop } from 'vue-property-decorator';
     }
 })
 export default class AppComponent extends Vue {
-    public drawer: boolean = false;
+    public activeRoute: string = '';
+    public drawer: boolean = true;
+    public routes: any = [
+        {
+            route: 'blog',
+            action: null,
+            title: 'Blog'
+        },
+        {
+            route: 'counter',
+            action: null,
+            title: 'Counter'
+        },
+    ];
 
     toggleDrawer() {
-        this.drawer = !this.drawer;
+        this.drawer = true;
+    }
+
+    @Watch('$route')
+    onRoutwChanged(val: any, oldVal: any) {
+        this.activeRoute = val.fullPath;
+    }
+
+    mounted() {
+        this.activeRoute = this.$route.fullPath;
     }
 }
